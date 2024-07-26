a5d6e671b84c7126dd5434b9f0118bfc6a7a5b12
irem <iremhverdi@gmail.com>
2024-07-26 16:46:07 +0300

    selected operation
    seçilen işarete göre işlem yapılan kod

---
+++ deneme.js
@@ -0,0 +1,32 @@
+function calculate(a,b,operator){ //function for calculate the numbers with selected operator
+    let ans="";//variable for answer
+    switch(operator){//attitude towards the selected operator
+        case "+":
+            ans= a+b;
+            break;
+        case "-":
+            ans= a-b;
+            break;
+        case "/":
+            ans= a/b;  
+            break;
+        case "*":
+            ans= a*b; 
+            break;
+        default:
+            console.log("Invalid operator, please enter one of the operators (+,-,*,/)");
+            return null;
+    }
+    return ans;
+}
+
+function main(){
+    const num1= prompt("Enter the first number to calculate: ");//usage of prompt for getting the info from user
+    const num2= prompt("Enter the second number to calculate: ");
+    const operator= prompt ("Enter a calculation operator (+,-,*,/): ");
+    const answer = calculate(num1, num2, operator);
+    if (answer !== null) {
+        console.log("Answer is: " +num1 + ""+ operator+ ""+ num2 +"=" +answer);
+      }
+}
+main();//manually calling the main function,unlike other languages
